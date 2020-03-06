edabddc86f759214a5cb83ac901b63a637991e34
landaguo <lan_daguo@163.com>
2020-03-06 17:27:06 +0800

    # A1001

--- src/A/A1001.java
+++ src/A/A1001.java
@@ -1,18 +1,64 @@
 package A;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 /**
- * @author ：junshanglan
- * @date ：Created in 2019/12/29 9:34
- * @description：
+ * Calculate a+b and output the sum in standard format -- that is,
+ * the digits must be separated into groups of three by commas
+ * (unless there are less than four digits).
+ *
+ * input:
+ * -1000000 9
+ * output:
+ * -999,991
+ *
  */
 public class A1001 {
 
+    private static final char MINUS = '-';
 
+    private static final int SEPARATE = 3;
+
+    /**
+     * 负数的时候要注意逗号的位置
+     * -998
+     * -1,000
+     * -1,000,234
+     * -10256
+     * @param args
+     */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] input= sc.nextLine().split(" ");
+        Integer A = Integer.valueOf(input[0]);
+        Integer B = Integer.valueOf(input[1]);
+        String result = A + B + "";
 
+        // 特殊的情况直接输出
+        if (result.length() <= 3) {
+            System.out.println(result);
+        }else {
+            int length = result.length();
+            if (result.charAt(0) == '-') {
+                length--;
+            }
+            // 计算当前的sum要执行次数
+            int iterateCount = length / 3;
+            int separateIndex ;
+            for (int i = 0; i < iterateCount; i++) {
+                // 计算要分割的位置：逗号个数 + 循环次数 * 3
+                separateIndex = result.length() - SEPARATE * (i + 1) - i;
+                // 判断分割的前一个字符是不是 "-" 号
+                char flag = result.charAt(separateIndex - 1);
+                if (MINUS == flag) {
+                    break;
+                }else {
+                    result = result.substring(0, separateIndex) + "," + result.substring(separateIndex, result.length());
+                }
+            }
+            System.out.println(result);
+
+        }
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,6 @@
+import java.util.Scanner;
 
-import java.util.*;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-
-/**
- * @author ：junshanglan
- * @date ：Created in 2019/12/29 9:34
- * @description：
- */
 public class Main {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        List<Integer> collect = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::valueOf).collect(Collectors.toList());
-        collect.remove(0);
-
-        int count = Integer.valueOf(scanner.nextLine());
-        for (int i = 0; i < count; i++) {
-            List<Integer> data = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::valueOf).collect(Collectors.toList());
-            List<Integer> result = new ArrayList<>();
-            Integer begin = data.get(0);
-            Integer end = data.get(1);
-            if (begin < end) {
-                sequence(collect,result,begin,end);
-                reverse(collect,result,end,begin);
-            }
-
-            if (begin > end) {
-                reverse(collect,result,begin,end);
-                sequence(collect,result,end,begin);
-            }
-            System.out.println(Collections.min(result));
-        }
-    }
-
-    private static void reverse(List<Integer> collect,List<Integer> result,Integer begin,Integer end){
-        Integer sum = 0;
-        for (int j = begin; j <= collect.size();j++) {
-            sum += collect.get(j - 1);
-        }
-        for (int j = 0; j < end -1; j++) {
-            sum += collect.get(j);
-        }
-        result.add(sum);
-    }
-
-    private static void sequence(List<Integer> collect,List<Integer> result,Integer begin,Integer end){
-        Integer sum = 0;
-        for (int j = begin; j < end; j++) {
-            sum += collect.get(j - 1);
-        }
-        result.add(sum);
-    }
-
-
 
 
 }
